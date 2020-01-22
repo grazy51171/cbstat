14b4164029940330313924491c4d4618c89c359a
Grazy <grazy51171@gmail.com>
2020-01-22 18:11:10 +0100

    Remove analytic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
 import { Component } from '@angular/core';
 
-declare const firebase: any;
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor() {
-    try {
-      firebase.analytics();
-    } catch (err) {}
-  }
+  constructor() {}
 }
