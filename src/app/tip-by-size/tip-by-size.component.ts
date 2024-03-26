34699ab48c8768cc94f3aa4888f87eb09fcdee2b
Grazy <grazy51171@gmail.com>
2024-03-26 16:52:52 +0100

    Tip by size update

--- src/app/tip-by-size/tip-by-size.component.ts
+++ src/app/tip-by-size/tip-by-size.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataset } from 'chart.js';
-import { UntypedFormGroup, UntypedFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 import { DateTime } from 'luxon';
 import { from, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { StatTokenService } from '../stat-token.service';
 import { BaseChartDirective } from 'ng2-charts';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatExpansionModule } from '@angular/material/expansion';
 import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
+
+import { StatTokenService } from '../stat-token.service';
 
 @Component({
   standalone: true,
@@ -29,7 +29,6 @@ import { MatInputModule } from '@angular/material/input';
     MatRadioModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    MatExpansionModule,
     MatInputModule,
     MatDatepickerModule,
   ],
@@ -78,35 +77,32 @@ export class TipBySizeComponent implements OnInit {
   public chartType: ChartType = 'bubble';
   public chartLegend = true;
 
-  public graphOptions: UntypedFormGroup;
-
   public dateLimits: Observable<{ first: Date; last: Date }>;
 
-  private readonly defaultOptions = {
-    typeUser: 'tipper',
-    chartType: 'bubble' as ChartType,
-    dateMin: null as DateTime,
-    dateMax: null as DateTime,
-  };
-  constructor(private statToken: StatTokenService, formBuilder: UntypedFormBuilder) {
-    this.graphOptions = formBuilder.group(this.defaultOptions);
-  }
+  public graphOptions = new FormGroup({
+    typeUser: new FormControl('tipper'),
+    chartType: new FormControl('bubble' as ChartType),
+    dateMin: new FormControl(null as DateTime),
+    dateMax: new FormControl(null as DateTime),
+  });
+
+  constructor(private statToken: StatTokenService) {}
 
   ngOnInit() {
     this.dateLimits = from(this.statToken.limits());
-    this.updateGraph(this.defaultOptions);
+    this.updateGraph(this.graphOptions.value);
     this.graphOptions.valueChanges
       .pipe(
         map((v) =>
           Object.assign({}, v, {
-            dateMax: DateTime.fromJSDate(v.dateMax).plus({ day: 1 }).toJSDate(),
+            dateMax: v?.dateMax?.plus({ day: 1 }),
           })
         )
       )
       .subscribe((val) => this.updateGraph(val));
   }
 
-  private updateGraph(val: { chartType: ChartType; typeUser: string; dateMin: DateTime; dateMax: DateTime }) {
+  private updateGraph(val: Partial<{ chartType: ChartType; typeUser: string; dateMin: DateTime; dateMax: DateTime }>) {
     const sizes = [1, 10, 20, 50, 100, 200, 500, 1000, 1000000];
     this.chartType = val.chartType;
     this.statToken.tipBySize(val.typeUser !== 'tipper', val.dateMin, val.dateMax, sizes).then((tipBySize) => {
